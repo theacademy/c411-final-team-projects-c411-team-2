b041b7430d9dc04ab39f9f4b5fdb049b023596ce
thereelslimshady <jacynthe_tessier@outlook.com>
2025-03-24 17:10:58 -0400

    fixed password

--- src/main/resources/travel-itinerary-schema.sql
+++ src/main/resources/travel-itinerary-schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `travel-itinerary`.`user` (
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(100) NOT NULL,
-  `password` VARCHAR(30) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
   `origin_city` VARCHAR(50) NULL,
   `dob` DATE NULL,
   PRIMARY KEY (`id`))

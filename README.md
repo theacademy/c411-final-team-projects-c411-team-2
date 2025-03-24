beab13f1e891e003b23f310377d41d5232cdf60f
Loretana <lmgsbusiness@gmail.com>
2025-03-24 12:42:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Build-A-Trip
+
+## Members:
+ - Hamzah Muhammad  
+ - Jacynthe Tessier
+ - Laura Ladaru
+ - Sean Lau-Ming
---
+++ src/main/java/org/buildATrip/App.java
@@ -0,0 +1,9 @@
+package org.buildATrip;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Build-A-Trip");
+    }
+
+}

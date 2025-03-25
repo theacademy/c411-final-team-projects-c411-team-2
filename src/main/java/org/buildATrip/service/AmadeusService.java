0a8b613502079630ff250c291921f8e379b04f8d
thereelslimshady <jacynthe_tessier@outlook.com>
2025-03-25 14:17:21 -0400

    started mapper on flightsearchoffer

--- src/main/java/org/buildATrip/service/AmadeusService.java
+++ src/main/java/org/buildATrip/service/AmadeusService.java
@@ -11,7 +11,7 @@ public interface AmadeusService {
     Flight[] getFlights(String originaLocationCode, String destinationLocationCode, LocalDate departureDate, LocalDate returnDate, int numberAdults, int maxPrice, boolean isNonStop ) throws ResponseException;
     //https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search/api-reference
 
-    Flight[] getFlightsByDestination(String originaLocationCode, String destinationLocationCode, LocalDate departureDate, int duration, int numberAdults, int maxPrice, boolean isNonStop );
+    Flight[] getFlightsByDestination(String originLocationCode, String destinationLocationCode, LocalDate departureDate, int duration, int numberAdults, int maxPrice, boolean isNonStop ) throws ResponseException;
     //https://developers.amadeus.com/self-service/category/flights/api-doc/flight-inspiration-search/api-reference
 
     Hotel[] getHotelsByCity(String cityCode, int numberAdults, LocalDate checkinDate, LocalDate checkoutDate, String priceRange, BoardType boardType);
--- src/main/java/org/buildATrip/service/AmadeusServiceImpl.java
+++ src/main/java/org/buildATrip/service/AmadeusServiceImpl.java
@@ -1,10 +1,13 @@
 package org.buildATrip.service;
 
+import com.amadeus.resources.FlightDestination;
 import com.amadeus.resources.FlightOfferSearch;
 import com.amadeus.shopping.FlightOffers;
 import org.buildATrip.entity.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import com.amadeus.Amadeus;
 import com.amadeus.Params;
 import com.amadeus.exceptions.ResponseException;
@@ -39,17 +42,41 @@ public class AmadeusServiceImpl implements AmadeusService {
                         .and("departureDate", departureDate)
                         .and("returnDate", returnDate)
                         .and("adults", numberAdults)
+                        .and("children", 0)
+                        .and("infants", 0)
                         .and("nonStop", isNonStop)
                         .and("maxPrice", maxPrice)
                         .and("currencyCode", "CAD")
                         .and("max", 3));
+        // the date on which the traveler will depart from the destination to return to the origin. If this parameter is not specified, only one-way itineraries are found. If this parameter is specified, only round-trip itineraries are found. Dates are specified in the ISO 8601 YYYY-MM-DD format, e.g. 2018-02-28
+        for (FlightOfferSearch flightOffer : flightOffers) {
+            flightOffer.getPrice();
+            FlightOfferSearch.SearchSegment[] segmentsOneWay = flightOffer.getItineraries()[0].getSegments();
+            for (FlightOfferSearch.SearchSegment segment : segmentsOneWay) {
+                segment.getDeparture().getIataCode();
+                LocalDateTime.parse(segment.getDeparture().getAt());
+                LocalDateTime.parse(segment.getArrival().getIataCode());
+                segment.getDuration();
+            }
+        }
         return null;
     }
 
 
     @Override
-    public Flight[] getFlightsByDestination(String originaLocationCode, String destinationLocationCode, LocalDate departureDate, int duration, int numberAdults, int maxPrice, boolean isNonStop) {
-        return new Flight[0];
+    public Flight[] getFlightsByDestination(String originLocationCode, String destinationLocationCode, LocalDate departureDate, int duration, int numberAdults, int maxPrice, boolean isNonStop) throws ResponseException {
+        FlightDestination[] flightDestinations = amadeus.shopping.flightDestinations.get(
+                Params.with("origin", originLocationCode)
+                        .and("departureDate", departureDate)
+                        .and("duration", duration)
+                        .and("oneWay", false)
+                        .and("nonStop", isNonStop)
+                        .and("maxPrice", maxPrice)
+                        .and("viewBy", "DESTINATION")
+
+        );
+        for ()
+            return new Flight[0];
     }
 
     @Override
@@ -66,7 +93,5 @@ public class AmadeusServiceImpl implements AmadeusService {
     public PointOfInterest[] getPointsOfInterestByCoordinates(float latitude, float longitude, ActivityType[] activityTypes) {
         return new PointOfInterest[0];
     }
-
-    //flightMapper
 
 }

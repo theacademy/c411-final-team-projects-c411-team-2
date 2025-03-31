78c7df1dc8cbd9d7a5949979288b02abffb4a61c
Hamzah Muhammad <99919629+hamzahmhd@users.noreply.github.com>
2025-03-31 11:38:16 -0400

    random commit

--- frontend/frontend/src/pages/SearchHotelsPage.jsx
+++ frontend/frontend/src/pages/SearchHotelsPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { searchHotel, addHotelToItinerary } from '../services/hotelService';
-import {addActivityToItinerary} from "../services/activityService.js";
+
 
 const SearchHotelsPage = () => {
     const { itineraryId } = useParams();
